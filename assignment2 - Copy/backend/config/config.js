10f2d9c147a22eb89c67817777112fc6ddda5914
Zackfeng02 <113264902+Zackfeng02@users.noreply.github.com>
2025-02-26 10:13:24 -0500

    DELETE

--- assignment2 - Copy/backend/config/config.js
+++
@@ -1,9 +0,0 @@
-// Exporting an object that contains the MongoDB URI and the JWT secret key
-// The MongoDB URI is stored in the MONGO_URI environment variable
-require('dotenv').config();
-
-module.exports = {
-  mongoURI: process.env.MONGO_URI || "mongodb://localhost:27017/student_course_db",
-  jwtSecret: process.env.JWT_SECRET || "my-32-character-ultra-secure-and-ultra-long-secret",
-  port: process.env.PORT || 5000
-};
--- assignment2 - Copy/backend/utils/validators.js
+++
@@ -1,9 +0,0 @@
-// Description: Contains all the validation functions.
-// The isValidEmail function checks if the email is valid.
-module.exports = {
-    isValidEmail: function(email) {
-      const re = /\S+@\S+\.\S+/;
-      return re.test(email);
-    }
-  };
-  
