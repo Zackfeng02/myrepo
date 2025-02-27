748364a8103d4174ac3ce72319baee5dfad026da
Zackfeng02 <113264902+Zackfeng02@users.noreply.github.com>
2025-02-27 10:55:05 -0500

    debug

--- assignment2/backend/models/Student.js
+++ assignment2/backend/models/Student.js
@@ -1,23 +1,16 @@
 const mongoose = require('mongoose');
 
 const studentSchema = new mongoose.Schema({
-  studentNumber: { type: String, required: true, unique: true },
+  studentNumber: { type: String, required: true },
   password: { type: String, required: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   program: { type: String, required: true },
-  courses: [{ 
-    type: mongoose.Schema.Types.ObjectId, 
-    ref: 'Course' ,
-    validate: {
-      validator: async function(courseId) {
-        const course = await mongoose.model('Course').findById(courseId);
-        return !!course;
-      },
-      message: 'Course does not exist'
-    }
-  }]
+  address: { type: String, required: true },
+  city: { type: String, required: true },
+  phone: { type: String, required: true },
+  courses: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Course' }]
 });
 
 module.exports = mongoose.model('Student', studentSchema);
